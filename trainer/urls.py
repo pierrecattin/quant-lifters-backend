1a15c053ab61f2985fae425b30a6cda438afa8ba
Pierre Cattin <cattin.pierre@gmail.com>
2024-01-29 17:29:11 +0100

    dummy post

--- trainer/urls.py
+++ trainer/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("exercise/<exercise_name>/", views.GetExercise, name="exercise"),
     path("allexercises/", views.AllExercisesNames, name="allexercises"),
+    path("addexercise", views.AddExercise, name="addexercise"),
 ]
--- trainer/views.py
+++ trainer/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from json import dumps
-from trainer.models import Exercise
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser 
+from rest_framework.decorators import api_view
 
+
+from trainer.models import Exercise, Workout
+
+@api_view(['GET'])
 def GetExercise(request, exercise_name:str):
     exercise = get_object_or_404(Exercise, pk=exercise_name)
     return HttpResponse(dumps(exercise.serialize()))
 
+@api_view(['GET'])
 def AllExercisesNames(request):
     exercises = Exercise.objects.all()
     return HttpResponse (dumps([e.serialize() for e in exercises]))
+
+@api_view(['POST'])
+@csrf_exempt
+def AddExercise(request):
+    data = JSONParser().parse(request)
+    exercise = Exercise(name=data['name'])
+    exercise.save()
+    return HttpResponse(dumps(exercise.serialize())) 
+    
